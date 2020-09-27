92763ceb34855a4da1490b46cf7271f3bc2d2e75
ambikeshwar1991 <srivastava.ambikeshwar@gmail.com>
2020-09-27 12:47:18 +0530

    refactored

--- backend/predict_api.py
+++ backend/predict_api.py
@@ -13,13 +13,12 @@ model = joblib.load('../fx_predict_model.model')
 class MakePrediction(Resource):
     @staticmethod
     def post():
-        # posted_data = request.get_json()
-        # print(posted_data)
-        # ird = posted_data['ird']
-        # cpi_diff = posted_data['cpi_diff']
-        # gdp_diff = posted_data['gdp_diff']
+        posted_data = request.get_json()
+        ird = float(posted_data["ird"])
+        cpi_diff = float(posted_data["cpi_diff"])
+        gdp_diff = float(posted_data["gdp_diff"])
 
-        prediction = model.predict([[1, 1, 1]])[0]
+        prediction = model.predict([[ird, cpi_diff, gdp_diff]])[0]
 
         return jsonify({
             'Prediction': prediction
