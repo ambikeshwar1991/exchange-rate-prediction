88a6bad965c28a114ca8babc4cf26e50958f81a4
ambikeshwar1991 <srivastava.ambikeshwar@gmail.com>
2020-10-06 14:12:21 +0530

    fixed bug

--- backend/predict_api.py
+++ backend/predict_api.py
@@ -1,11 +1,12 @@
 
 import os
 import joblib
-
+from flask_cors import CORS, cross_origin
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 model = joblib.load('../fx_predict_model.model')
@@ -14,14 +15,15 @@ class MakePrediction(Resource):
     @staticmethod
     def post():
         posted_data = request.get_json()
-        ird = float(posted_data["ird"])
-        cpi_diff = float(posted_data["cpi_diff"])
-        gdp_diff = float(posted_data["gdp_diff"])
+        print(posted_data)
+        ird = round(float(posted_data["interestRateDifferential"])) #interestRateDifferential
+        cpi_diff = round(float(posted_data["cpiDifference"]))
+        gdp_diff = round(float(posted_data["gdpDifference"]))
 
         prediction = model.predict([[ird, cpi_diff, gdp_diff]])[0]
-
+        #score_m = model.score([[ird, cpi_diff, gdp_diff]])
         return jsonify({
-            'Prediction': prediction
+            'Prediction': prediction,
         })
 
 
@@ -29,5 +31,5 @@ api.add_resource(MakePrediction, '/predict')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
 
